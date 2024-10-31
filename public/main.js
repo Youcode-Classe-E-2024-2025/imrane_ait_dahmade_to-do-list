b64b7c77ae7a418faaac3019ba21a02a2a06ba53
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-10-31 15:59:33 +0100

    utilisation de fonction event.preventdefault pour arreter l actiliser la page

--- public/main.js
+++ public/main.js
@@ -1,9 +1,27 @@
-const btn = document.getElementById('add');
-const formModal = document.getElementById('form-modal');
+document.addEventListener('DOMContentLoaded', function() {
+    const add = document.getElementById('add');
+    const formModal = document.getElementById('form-modal');
+    const confermer = document.getElementById('confirmer');
+    const cancel = document.getElementById('cancel');
+    const taches = {
+        title: document.getElementById('tilte_tache'), 
+        description: document.getElementById('description'),
+        date: document.getElementById('date'),
+    };
 
+    add.addEventListener('click', function() {
+        formModal.classList.remove('hidden');
+    });
 
-btn.addEventListener('click',function (){
- formModal.classList.remove('hidden');
-    
+    confermer.addEventListener('click', function(event) {
+        event.preventDefault();
+        let titleCopy = taches.title.value;
+        let descriptionCopy = taches.description.value;
+        let dateCopy = taches.date.value;
+        formModal.classList.add('hidden');
 
+    });
+    cancel.addEventListener('click', function() {
+        formModal.classList.add('hidden');
+    });
 });
