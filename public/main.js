a34f37174237ac9593371a0ae07bac6f59ae1105
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-11-01 10:33:30 +0100

    commit

--- public/main.js
+++ public/main.js
@@ -11,9 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     add.addEventListener('click', function () {
         formModal.classList.remove('hidden');
+        taches.title.value= '';
+        taches.description.value = '';
+        taches.date.value ='';
     });
 
     confermer.addEventListener('click', function (event) {
+       
         event.preventDefault();
         let titleCopy = taches.title.value;
         let descriptionCopy = taches.description.value;
@@ -22,18 +26,36 @@ document.addEventListener('DOMContentLoaded', function () {
         formModal.classList.add('hidden');
 
         let box = document.createElement('div');
-        let title_t = document.createElement('h1');
-        let description = document.createElement('p');
-        let datec = document.createElement('p');
-        to_do.appendChild(box);
-        box.appendChild(title_t);
-        title_t.innerText =titleCopy;
-        box.appendChild(description);
-        description.innerText = descriptionCopy;
-        box.appendChild(datec);
-        datec.innerText = dateCopy;
-        console.log(box);
-              
+        box.innerHTML = `
+            <h1>${titleCopy}</h1>
+            <p>${descriptionCopy}</p>
+            <p>${dateCopy}</p>
+        `
+        box.classList.add("flex-col");
+
+        // let title_t = document.createElement('h1');
+        // let description = document.createElement('p');
+        // let datec = document.createElement('p');
+        let select = document.getElementById('to_do_options');
+
+        if (select.value == 1) {
+                to_do.appendChild(box);
+        }
+
+
+        // if (select.value == 1) {
+        //     to_do.appendChild(box);
+        //     box.appendChild(title_t);
+        //     title_t.innerText = titleCopy;
+        //     box.appendChild(description);
+        //     description.innerText = descriptionCopy;
+        //     box.appendChild(datec);
+        //     datec.innerText = dateCopy;
+        //     console.log(box);
+        // }
+        // else if (select.value == 2){
+         
+        // }
     });
     cancel.addEventListener('click', function () {
         formModal.classList.add('hidden');
